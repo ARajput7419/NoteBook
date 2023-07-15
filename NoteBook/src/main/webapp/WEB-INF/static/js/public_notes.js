0e3fae19a1fa7e31c98e0895f312657dc91ac94e
Aniket Rajput <aniket.mca21@cs.du.ac.in>
2023-07-15 14:58:17 +0530

    project commit 36

--- NoteBook/src/main/webapp/WEB-INF/static/js/public_notes.js
+++ NoteBook/src/main/webapp/WEB-INF/static/js/public_notes.js
@@ -5,12 +5,85 @@ function render_notes(page_retrived,total_pages,list_notes,keyword){
     let start_page_field = document.getElementById("start_page");
     let start_page = Number(start_page_field.value);
 
+    if(start_page > 1){
+        document.getElementsByClassName("p0")[0].classList.remove("disabled");
+    }
+    else{
+        document.getElementsByClassName("p0")[0].classList.add("disabled");
+    }
+
+    if(total_pages>=start_page+3){
+        document.getElementsByClassName("p4")[0].classList.remove("disabled");
+    }
+    else{
+        document.getElementsByClassName("p4")[0].classList.add("disabled");
+    }
+
+    for( let i = 1;i<=3;i++){
+        let n  =Number(document.getElementsByClassName(`pp${i}`)[0].innerText);
+        if(total_pages<n){
+            document.getElementsByClassName(`p${i}`)[0].classList.add("disabled");
+            document.getElementsByClassName(`p${i}`)[0].classList.remove("active");
+        }
+        else{
+            document.getElementsByClassName(`p${i}`)[0].classList.remove("disabled");
+        }
+    }
+
+
+
     if(total_pages == 0){
         toast("No Notes Found");
+        let parent = document.getElementsByClassName("parent")[0];
+        parent.innerHTML = "";
         return;
     }
     else{
-        
+        if(list_notes.length == 0){
+
+            let offset = page_retrived - start_page + 1;
+            let page_item = document.getElementsByClassName(`p${offset}`)[0];
+            page_item.classList.remove("active");
+            page_item.classList.add("disabled");
+            for(let a = offset+1 ; a<=4;a++)
+            {
+                let next_page = document.getElementsByClassName(`p${a}`)[0];
+                next_page.classList.add("disabled");
+            }
+            
+            if(page_retrived == start_page){
+
+                if(start_page == 1){
+
+                    document.getElementsByClassName("p0").classList.add("disabled");
+                    toast("No Notes Found");
+                    let parent = document.getElementsByClassName("parent")[0];
+                    parent.innerHTML = "";
+                    return;
+
+                }
+
+                else
+                {
+
+                  document.getElementsByClassName("pp1")[0].innerText = start_page - 1;
+                  document.getElementsByClassName("pp2")[0].innerText = start_page ;
+                  document.getElementsByClassName("pp3")[0].innerText = start_page + 1;
+                  let start_field = document.getElementById("start_page");
+                  let current_field = document.getElementById("current_page");
+                  start_field.value = start_page - 1 ;
+                  current_field.value =  start_page - 1;
+                  
+                }
+
+
+            }
+
+            fetch_public_notes(page_retrived-1,keyword);
+
+            return;
+        }
+        else {
     
 
         let parent = document.getElementsByClassName("parent")[0];
@@ -24,79 +97,71 @@ function render_notes(page_retrived,total_pages,list_notes,keyword){
 
             let cardHTML = `
                             
-                            <div class="card card-spacing notes-card">
-                                <div class="card-body">
-                                <h5 class="card-title">${note.name}</h5>
-                                <p class="card-text visibility_${note.id}"><span>Visibility:</span> ${note.visibility}</p>
-                                <p class="card-text"><span>Author:</span> ${note.user.name}</p>
-                                <hr>
-                                <div class="card-buttons">
-                                
-                                    <a href="/notes/view/${note.id}" class="btn btn-primary">Read</a>
+            <div class="card card-spacing notes-card">
+                <div class="card-body">
+                <h5 class="card-title">${note.name}</h5>
+                <p class="card-text visibility_${note.id}"><span>Visibility:</span> ${note.visibility}</p>
+                <p class="card-text"><span>Author:</span> ${note.user.name}</p>
+                <hr>
+                <div class="card-buttons">
                 
-                                </div>
-                                    <p class="card-timestamp"><span>Time Stamp:</span> ${note.timestamp}</p>
-                                </div>
-                            </div>`;
+                    <a href="/notes/view/${note.id}" class="btn btn-primary">Read</a>
 
-            console.log(cardHTML);
+                </div>
+                    <p class="card-timestamp"><span>Time Stamp:</span> ${note.timestamp}</p>
+                </div>
+            </div>`;
+
+           
 
             parent.insertAdjacentHTML("beforeend",cardHTML);
         
         
         } 
         
-        // if( page_retrived != null)
-        // {
-        //     let offset = page_retrived - start_page + 1;
-        //     let page_item = document.getElementsByClassName(`p${offset}`);
-        //     page_item.classList.add("active");
-        // }
+        if( page_retrived != null)
+        {
+            let offset = page_retrived - start_page + 1;
+            let page_item = document.getElementsByClassName(`p${offset}`)[0];
+            page_item.classList.add("active");
+        }
+
+
+        }
 
     }   
 
 }
 
 
+async function fetch_public_notes(page_number,keyword){
 
 
-
-
-
-
-
-function fetch_public_notes(page_number,keyword){
+   
 
     let new_data = fetch(`/api/notes/public/${page_number}?keyword=${keyword}`);
-            new_data.then((response)=>{
+    let response_promise = new_data.then((response)=>{
                 if(response.ok)
                 return response.json();
                 else 
                 throw new Error("Not Able to Fetch Notes");
-            })
-            .then((res)=>{
-
-                let total_pages = res["total_pages"];
-                let list_notes = res["notes"];
-                render_notes(page_number,total_pages,list_notes,keyword);
-
-            }).catch((error)=>{
-                toast(error);
             });
+
+      let render_promise = response_promise.then((res)=>{
+                let total_pages = res["total_pages"];
+                render_notes(page_number,total_pages,res,keyword);
+                return total_pages;
+
+            });
+
+    return await render_promise;
+  
 
 }
 
 
 
-
-
-
-
-
-
-
 function pagination(offset,total_pages){
-
 
     total_pages = Number(total_pages);
     let page_item = document.getElementsByClassName(`p${offset}`)[0];
@@ -239,15 +304,20 @@ function pagination(offset,total_pages){
 }
 
 
-function searchNotes(){
 
-   
+
+
+
+
+
+
+  async function searchNotes(){
 
     let keyword = document.getElementById("notes_search").value;
 
-    let status = fetch_public_notes(1,keyword);
-    
-    if(status){
+    let total_pages = await fetch_public_notes(1,keyword);
+
+    if(total_pages){
 
         let keyword_field = document.getElementById("keyword");
         let start_field = document.getElementById("start_page");
@@ -255,11 +325,38 @@ function searchNotes(){
         keyword_field.value = keyword;
         start_field.value = 1;
         current_field.value = 1;
-     
+        for(let i = 1;i<=3;i++)
+        {
+                document.getElementsByClassName(`pp${i}`)[0].innerText = i;
+                
+                if(total_pages<i)
+                { 
+                    document.getElementsByClassName(`p${i}`)[0].classList.add("disabled");
+                    document.getElementsByClassName(`p${i}`)[0].classList.remove("active");
+            }
+            else{
+
+                document.getElementsByClassName(`p${i}`)[0].classList.remove("disabled");
+                document.getElementsByClassName(`p${i}`)[0].classList.remove("active");
+                
+            }
+
+        }
+       
+        document.getElementsByClassName("p1")[0].classList.add("active");
+
+        document.getElementsByClassName("p0")[0].classList.add("disabled");
+
+        if(total_pages<=3)
+        document.getElementsByClassName("p4")[0].classList.add("disabled");
+        else
+        document.getElementsByClassName("p4")[0].classList.remove("disabled");
+
     }
 
 
   }
+
 
   function makePostRequest(url,token){
 
@@ -277,3 +374,8 @@ function searchNotes(){
     f.submit();
 
 }
+
+
+
+
+
